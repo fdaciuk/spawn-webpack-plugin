c93d18716f71fcfd9a67c5b602cf556f4ef86ce0
Fernando Daciuk <f.daciuk@gmail.com>
2017-08-17 21:49:19 -0300

    Add first inplementation of plugin

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+class SpawnWebpackPlugin {
+  constructor ({ command, spawn, sync } = {}) {
+    this.command = command
+    this.sync = sync || false
+    this.spawn = spawn || require('child_process').spawn
+
+    this.emit = (compilation, callback) => {
+      this.exec(this.command)
+      return callback && callback()
+    }
+  }
+
+  apply (compiler) {
+    const event = this.sync ? 'compilation' : 'emit'
+    compiler.plugin(event, this.emit)
+  }
+
+  exec (command) {
+    return new Promise((resolve, reject) => {
+      const [program, ...params] = command.split(' ')
+      const cmd = this.spawn(program, params, { stdio: 'inherit' })
+      cmd.on('close', resolve)
+      cmd.on('error', reject)
+    })
+  }
+}
+
+module.exports = SpawnWebpackPlugin
